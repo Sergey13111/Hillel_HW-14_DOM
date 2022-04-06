8076f373ae3af566923ca7dbbd553f61aa730a76
Sergey13111 <boiko.sergey93@gmail.com>
2022-04-06 13:24:53 +0300

    Edits scriptEdits script

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 "use strict";
 
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.append(el);
+}
+
 const getAlboms = async () => {
     try {
-        function createNode(element) {
-            return document.createElement(element);
-        }
-        
-        function append(parent, el) {
-          return parent.appendChild(el);
-        }
-
         const ul = document.getElementById('albums');
 
         const response = await fetch("https://jsonplaceholder.typicode.com/albums");
         const albums = await response.json();
         return albums.map(function(album) {
             const li = createNode("li");
-            li.innerHTML = `${album.title}`;
-            append(ul, li);
+            li.innerHTML = album.title;
             li.classList.add("album_title");
+            append(ul, li);
         });
     } catch (error) {
         return console.log(error);
